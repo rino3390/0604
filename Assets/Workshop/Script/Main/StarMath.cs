440e3fabb654f1b42db128853b6e972eb24b8721
rino3390 <sherikawaii3390@gmail.com>
2021-06-08 18:20:37 +0800

    feat: add test

---
+++ Assets/Workshop/Script/Main/StarMath.cs
@@ -0,0 +1,7 @@
+namespace Main
+{
+	public class StarMath
+	{
+		public int Add(int number1, int number2) { return number1 + number2; }
+	}
+}
--- Assets/Workshop/Script/Tests/TestScript.cs
+++ Assets/Workshop/Script/Tests/TestScript.cs
@@ -1,16 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
+using Main;
 using NUnit.Framework;
 using UnityEngine;
-using UnityEngine.TestTools;
 
-public class TestScript
+namespace MainTest
 {
-	public string ii;
-	// A Test behaves as an ordinary method
-	[Test]
-	public void Math_Add_Positive_Test()
+	public class TestScript
 	{
-		new StarMath()
+		// A Test behaves as an ordinary method
+		[Test]
+		[TestCase(1, 2, 3)]
+		[TestCase(789, 123, 912)]
+		[TestCase(0,0,0)]
+		public void Math_Add_Positive_Test(int number1, int number2, int expected) {
+			var starMath = new StarMath();
+ 			var addResult = starMath.Add(number1, number2);
+			Assert.AreEqual(expected, addResult);
+		}
 	}
 }
