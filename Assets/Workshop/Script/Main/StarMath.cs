71ddbefe4f3ed81ea3f0dbbb5c20e5f9150b662e
rino3390 <sherikawaii3390@gmail.com>
2021-06-11 04:15:16 +0800

    Refactor extract method - IsNumberNegative

--- Assets/Workshop/Script/Main/StarMath.cs
+++ Assets/Workshop/Script/Main/StarMath.cs
@@ -5,11 +5,17 @@ namespace Main
 	public class StarMath
 	{
 		public int Add(int number1, int number2) {
-			if (number1 < 0 && number2 < 0) { throw new Exception("number1 and number2 is negative number."); }
-			if (number1 < 0) { throw new Exception("number1 is negative number."); }
-			if (number2 < 0) { throw new Exception("number2 is negative number."); }
+			var number1IsNegative = IsNumberNegative(number1);
+			var number2IsNegative = number2 < 0;
+			if (number1IsNegative && number2IsNegative) { throw new Exception("number1 and number2 is negative number."); }
+			if (number1IsNegative) { throw new Exception("number1 is negative number."); }
+			if (number2IsNegative) { throw new Exception("number2 is negative number."); }
 			var result = number1 + number2;
 			return result;
+		}
+
+		private bool IsNumberNegative(int number) {
+			return number < 0;
 		}
 	}
 }
