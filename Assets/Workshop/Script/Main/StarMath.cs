9d0f811a71807a05c217389a8d091413a21a4cd4
rino3390 <sherikawaii3390@gmail.com>
2021-06-11 03:40:42 +0800

    add test - Should_Throw_Exception by wrong number

--- Assets/Workshop/Script/Main/StarMath.cs
+++ Assets/Workshop/Script/Main/StarMath.cs
@@ -1,7 +1,15 @@
+using System;
+
 namespace Main
 {
 	public class StarMath
 	{
-		public int Add(int number1, int number2) { return number1 + number2; }
+		public int Add(int number1, int number2) {
+			if (number1 < 0 && number2 < 0) { throw new Exception("number1 and number2 is negative number."); }
+			if (number1 < 0) { throw new Exception("number1 is negative number."); }
+			if (number2 < 0) { throw new Exception("number2 is negative number."); }
+			var result = number1 + number2;
+			return result;
+		}
 	}
 }
--- Assets/Workshop/Script/Tests/TestScript.cs
+++ Assets/Workshop/Script/Tests/TestScript.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Main;
@@ -8,15 +9,30 @@ namespace MainTest
 {
 	public class TestScript
 	{
+		private StarMath _starMath;
+
+		[SetUp]
+		public void Setup() {
+			_starMath = new StarMath();
+		}
+
 		// A Test behaves as an ordinary method
 		[Test]
 		[TestCase(1, 2, 3)]
 		[TestCase(789, 123, 912)]
-		[TestCase(0,0,0)]
+		[TestCase(0, 0, 0)]
 		public void Math_Add_Positive_Test(int number1, int number2, int expected) {
-			var starMath = new StarMath();
- 			var addResult = starMath.Add(number1, number2);
+			var addResult = _starMath.Add(number1, number2);
 			Assert.AreEqual(expected, addResult);
+		}
+
+		[Test]
+		[TestCase(-1,0,"number1 is negative number.")]
+		[TestCase(0,-1,"number2 is negative number.")]
+		[TestCase(-1,-1,"number1 and number2 is negative number.")]
+		public void Should_Throw_Exception(int number1, int number2, string errorMessage) {
+			var exception = Assert.Throws<Exception>(() => { _starMath.Add(number1, number2); });
+			Assert.AreEqual(errorMessage, exception.Message);
 		}
 	}
 }
