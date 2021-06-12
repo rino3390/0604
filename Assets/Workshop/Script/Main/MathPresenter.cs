f96de58e94a9aeb9bd18004f7d77fef5aa99d3cf
rino3390 <sherikawaii3390@gmail.com>
2021-06-12 20:24:39 +0800

    feat : add main scene and binding button,inputfield,textresult

---
+++ Assets/Workshop/Script/Main/MathPresenter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Serialization;
+using UnityEngine.UI;
+
+namespace Main
+{
+	public class MathPresenter : MonoBehaviour
+	{
+		private StarMath  starMath;
+		[SerializeField]
+		private Button buttonCaculate;
+		[SerializeField]
+		private InputField inputNumber1;
+		[SerializeField]
+		private InputField inputNumber2;
+		[SerializeField]
+		private Text textResult;
+
+		private void Start() {
+			starMath = new StarMath();
+			buttonCaculate.onClick.AddListener(() => {
+				var number1 = int.Parse(inputNumber1.text);
+				var number2 = int.Parse(inputNumber2.text);
+				var result = starMath.Add(number1, number2);
+				textResult.text = result.ToString();
+			});
+		}
+	}
+}
